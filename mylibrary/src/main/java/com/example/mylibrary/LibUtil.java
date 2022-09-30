2acb84424c7461335b581f81efae930c27a37c8b
chenchao <k82479@nbcb.com>
2022-09-30 10:11:28 +0800

    [1]第一次提交

---
+++ mylibrary/src/main/java/com/example/mylibrary/LibUtil.java
@@ -0,0 +1,21 @@
+package com.example.mylibrary;
+
+/**
+ * {类说明}
+ * <p>
+ * update_{时间}_{项目编号}_{开发者}
+ * 描述：{描述}
+ * 风险：{风险}
+ * <p>
+ * create_2022/9/30 9:45_{项目编号}_K82479_chenchao
+ * 描述：{描述}
+ * 风险：{风险}
+ *
+ * @XXX
+ * @XXX
+ */
+public class LibUtil {
+    public static String print(){
+        return "我是LibUtil！！！";
+    }
+} 
